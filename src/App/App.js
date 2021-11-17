768d55b72f8e48fe42725e61abb23e371c32e08d
Luke Thinnes <lukethinnes@yahoo.com>
2021-11-16 17:40:06 -0700

    Added the day of the week.

--- src/App/App.js
+++ src/App/App.js
@@ -1,77 +1,87 @@
-import { useEffect, useState } from "react";
-import { WeatherDay } from '../Components/WeatherDay'
-import styles from '../styles/styles.module.css'
-import { apiKey } from '../constants.js'
-import { LocationSearch } from '../Components/LocationSearch.js'
+import {useEffect, useState} from 'react';
+import {WeatherDay} from '../Components/WeatherDay';
+import styles from '../styles/styles.module.css';
+import {apiKey} from '../constants.js';
+import {LocationSearch} from '../Components/LocationSearch.js';
 
 export const App = () => {
-
-  const [locationKey, setLocationKey] = useState('')
+  const [locationKey, setLocationKey] = useState('');
 
   const [weatherInfo, setWeatherInfo] = useState();
 
-const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('');
 
-  const padNumber = (number) => {
-    const stringNumber = number + ''
-    const stringLength = stringNumber.length
+  const padNumber = number => {
+    const stringNumber = number + '';
+    const stringLength = stringNumber.length;
 
     if (stringLength === 1) {
-      return '0' + stringNumber
+      return '0' + stringNumber;
     } else {
-      return stringNumber
+      return stringNumber;
     }
-  }
+  };
 
-	useEffect(() => {
+  useEffect(() => {
+    const daysOfWeek = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
     if (locationKey) {
       fetch(
-        `http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey=${locationKey}?apikey=${apiKey}`
+        `http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey=${locationKey}?apikey=${apiKey}`,
       )
-      .then(res => res.json())
-      .then(res => {
-        setWeatherInfo(res.DailyForecasts
-          .map(df => {
-          return {
-            min: df.Temperature.Minimum.Value,
-            max: df.Temperature.Maximum.Value,
-            weatherType: df.Day.IconPhrase,
-            weatherKey: padNumber(df.Day.Icon),
-          }
-        }))
-      })
+        .then(res => res.json())
+        .then(res => {
+          console.log(
+            daysOfWeek[new Date(res.DailyForecasts[0].Date).getDay()],
+          );
+          setWeatherInfo(
+            res.DailyForecasts.map(df => {
+              return {
+                min: df.Temperature.Minimum.Value,
+                max: df.Temperature.Maximum.Value,
+                weatherType: df.Day.IconPhrase,
+                weatherKey: padNumber(df.Day.Icon),
+                dayOfWeek: daysOfWeek[new Date(df.Date).getDay()],
+              };
+            }),
+          );
+        });
     }
-    }, [locationKey])
+  }, [locationKey]);
 
-	return (
-        <>
-        <div className={styles.header}>
-            FORECASTER
+  return (
+    <>
+      <div className={styles.header}>FORECASTER</div>
+      <div className={styles.searchContainer}>
+        <LocationSearch
+          onCityFound={cityInfo => {
+            setLocationKey(cityInfo.key);
+            setLocation(cityInfo.name + ', ' + cityInfo.state);
+          }}
+        />
+        <h1>{location}</h1>
+        <div className={styles.main}>
+          {!!weatherInfo &&
+            weatherInfo.map((i, index) => (
+              <div className={styles.day} key={index}>
+                <WeatherDay
+                  min={i.min}
+                  max={i.max}
+                  weatherType={i.weatherType}
+                  weatherKey={i.weatherKey}
+                  dayOfWeek={i.dayOfWeek}
+                />
+              </div>
+            ))}
         </div>
-  <div className={styles.searchContainer}>
-  <LocationSearch 
-    onCityFound={cityInfo => {
-      setLocationKey(cityInfo.key) 
-      setLocation(cityInfo.name + ', ' + cityInfo.state)
-    }}
-  />
-    <h1>{location}</h1>
-    <div className={styles.main}>
-      {!!weatherInfo && weatherInfo.map((i, index) => (
-        <div 
-        className={styles.day}
-        key={index}>
-          <WeatherDay 
-            min={i.min} 
-            max={i.max}   
-            weatherType={i.weatherType} 
-            weatherKey={i.weatherKey}
-          />
-        </div>
-      ))}
-    </div>
-  </div>
-        </>
+      </div>
+    </>
   );
-}
-
+};
--- src/Components/WeatherDay.js
+++ src/Components/WeatherDay.js
@@ -1,6 +1,7 @@
-export const WeatherDay = ({ date, min, max, weatherType, weatherKey }) => {
+export const WeatherDay = ({ date, dayOfWeek, min, max, weatherType, weatherKey }) => {
   return (
     <>
+      {dayOfWeek}
       <img 
         alt={weatherType}
         src={`https://developer.accuweather.com/sites/default/files/${weatherKey}-s.png`}  
